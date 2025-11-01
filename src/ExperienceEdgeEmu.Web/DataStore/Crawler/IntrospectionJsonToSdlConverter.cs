f7132aa5a76b8cebc88898b74447c4e806e2aa2c
Per Bering <pbering@fastmail.com>
2025-11-01 14:46:17 +0100

    moved converter

--- src/ExperienceEdgeEmu.Web/DataStore/Crawler/IntrospectionJsonToSdlConverter.cs
+++ src/ExperienceEdgeEmu.Web/DataStore/Crawler/IntrospectionJsonToSdlConverter.cs
@@ -2,7 +2,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace ExperienceEdgeEmu.Web.EmuSchema;
+namespace ExperienceEdgeEmu.Web.DataStore.Crawler;
 
 public class IntrospectionJsonToSdlConverter
 {
